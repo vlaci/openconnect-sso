a8fce5e05602b11b003dd8a0d370e02250f3b2c9
László Vaskó <laszlo.vasko@outlook.com>
2019-10-18 10:44:31 +0200

    app: removing a bunch of unneeded code

--- openconnect_sso/app.py
+++ openconnect_sso/app.py
@@ -1,29 +1,25 @@
-import asyncio
-import getpass
+import json
 import logging
-import os
 import signal
-from pathlib import Path
+import sys
 
 import structlog
-from prompt_toolkit import HTML
-from prompt_toolkit.eventloop import use_asyncio_event_loop
-from prompt_toolkit.shortcuts import radiolist_dialog
+
+from PyQt5.QtCore import QUrl, QTimer
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineScript
+from PyQt5.QtWidgets import QApplication
 
 from openconnect_sso import config
-from openconnect_sso.config import Credentials
-from openconnect_sso.saml_authenticator import authenticate_in_browser
 
-logger = structlog.get_logger()
+app = None
+logger = structlog.get_logger("webengine")
 
 
 def run(args):
     configure_logger(logging.getLogger(), args.log_level)
-    loop = asyncio.get_event_loop()
-    use_asyncio_event_loop(loop)
 
     try:
-        return asyncio.get_event_loop().run_until_complete(_run(args))
+        return run_browser_process(args)
     except KeyboardInterrupt:
         logger.warn("CTRL-C pressed, exiting")
 
@@ -49,8 +45,118 @@ def configure_logger(logger, level):
     logger.setLevel(level)
 
 
-async def _run(args):
-    logger.info(str(args))
-    session_token = await authenticate_in_browser(args.login_url, args.login_final_url, args.token_cookie_name)
+def run_browser_process(args):
+    signal.signal(signal.SIGTERM, on_sigterm)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    print(session_token)
+    # To work around funky GC conflicts with C++ code by ensuring QApplication terminates last
+    global app
+    configure_logger(logging.getLogger(), args.log_level)
+
+    cfg = config.load()
+
+    app = QApplication(sys.argv)
+
+    # In order to make Python able to handle signals
+    force_python_execution = QTimer()
+    force_python_execution.start(200)
+
+    def ignore():
+        pass
+
+    force_python_execution.timeout.connect(ignore)
+    web = WebBrowser(cfg.auto_fill_rules, args.login_final_url, args.token_cookie_name)
+
+    web.authenticate_at(QUrl(args.login_url), cfg.credentials)
+
+    web.show()
+    rc = app.exec_()
+
+    logger.info("Exiting browser")
+    return rc
+
+
+class WebBrowser(QWebEngineView):
+    def __init__(self, auto_fill_rules, login_final_url, token_cookie_name):
+        super().__init__()
+        self.login_final_url = login_final_url
+        self._auto_fill_rules = auto_fill_rules
+        cookie_store = self.page().profile().cookieStore()
+        cookie_store.cookieAdded.connect(self._on_cookie_added)
+        self.page().loadFinished.connect(self._on_load_finished)
+        self.token_cookie_name = token_cookie_name
+        self.cookies = {}
+
+    def authenticate_at(self, url, credentials):
+        if credentials:
+            logger.info("Initiating autologin", cred=credentials)
+            for url_pattern, rules in self._auto_fill_rules.items():
+                script = QWebEngineScript()
+                script.setInjectionPoint(QWebEngineScript.DocumentReady)
+                script.setWorldId(QWebEngineScript.ApplicationWorld)
+                script.setSourceCode(
+                    f"""
+// ==UserScript==
+// @include {url_pattern}
+// ==/UserScript==
+
+function autoFill() {{
+    {get_selectors(rules, credentials)}
+    setTimeout(autoFill, 1000);
+}}
+autoFill();
+"""
+                )
+                self.page().scripts().insert(script)
+
+        self.load(QUrl(url))
+
+    def _on_cookie_added(self, cookie):
+        logger.debug("Cookie set", name=to_str(cookie.name()))
+        self.cookies[to_str(cookie.name())] = to_str(cookie.value())
+
+
+    def _on_load_finished(self, success):
+        url = self.page().url().toString()
+        logger.debug("Page loaded", url=url)
+
+        if url == self.login_final_url:
+            sys.stdout.write(self.cookies[self.token_cookie_name])
+            sys.stdout.flush()
+            QApplication.quit()
+
+
+def to_str(qval):
+    return bytes(qval).decode()
+
+
+def get_selectors(rules, credentials):
+    statements = []
+    for i, rule in enumerate(rules):
+        selector = json.dumps(rule.selector)
+        if rule.action == "stop":
+            statements.append(
+                f"""var elem = document.querySelector({selector}); if (elem) {{ return; }}"""
+            )
+        elif rule.fill:
+            value = json.dumps(getattr(credentials, rule.fill, None))
+            if value:
+                statements.append(
+                    f"""var elem = document.querySelector({selector}); if (elem) {{ elem.dispatchEvent(new Event("focus")); elem.value = {value}; elem.dispatchEvent(new Event("blur")); }}"""
+                )
+            else:
+                logger.warning(
+                    "Credential info not available",
+                    type=rule.fill,
+                    possibilities=dir(credentials),
+                )
+        elif rule.action == "click":
+            statements.append(
+                f"""var elem = document.querySelector({selector}); if (elem) {{ elem.dispatchEvent(new Event("focus")); elem.click(); }}"""
+            )
+    return "\n".join(statements)
+
+
+def on_sigterm(signum, frame):
+    logger.info("SIGNAL handler")
+    QApplication.quit()
--- openconnect_sso/browser/__init__.py
+++
@@ -1,1 +0,0 @@
-from .browser import Browser
