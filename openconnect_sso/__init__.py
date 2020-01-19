b2c7d2411c6c49d60db0aced00e01318f65717b3
László Vaskó <laszlo.vasko@outlook.com>
2020-01-19 18:38:21 +0100

    openconnect-sso: single-sourcing package version

--- openconnect_sso/__init__.py
+++ openconnect_sso/__init__.py
@@ -1,2 +1,9 @@
-__version__ = "0.3.8"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    # Python < 3.8
+    import importlib_metadata
+
+
+__version__ = importlib_metadata.version('openconnect-sso')
 __description__ = "Wrapper script for OpenConnect supporting Azure AD (SAMLv2) authentication to Cisco SSL-VPNs"
