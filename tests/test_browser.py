fdef9ae641d400260f3144efbeafb2a0c59fc425
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-11-17 20:44:37 +0100

    Apply refurb suggestions
    [FURB109]: Replace `in [x, y, z]` with `in (x, y, z)`

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -12,7 +12,7 @@ async def test_browser_context_manager_should_work_in_empty_context_manager():
 
 
 @pytest.mark.xfail(
-    sys.platform in ["darwin", "win32"],
+    sys.platform in ("darwin", "win32"),
     reason="https://github.com/vlaci/openconnect-sso/issues/23",
 )
 @pytest.mark.asyncio
@@ -28,7 +28,7 @@ async def test_browser_reports_loaded_url(httpserver):
 
 
 @pytest.mark.xfail(
-    sys.platform in ["darwin", "win32"],
+    sys.platform in ("darwin", "win32"),
     reason="https://github.com/vlaci/openconnect-sso/issues/23",
 )
 @pytest.mark.asyncio
