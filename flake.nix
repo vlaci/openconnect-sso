8f438d82d463f3e3283eabbbf1bdbc80ec33d684
ThinkChaos <ThinkChaos@users.noreply.github.com>
2023-11-27 12:20:05 -0500

    fix(build): update nix flake to work with current NixOS unstable
    Niv appears to work too, but I haven't tested that as much.
    
    Changes (sorry single big commit, but this was hard enough as is):
    - don't use the overlay for nixpkgs since it's not used ATM
    - when using a flake, respect the given inputs instead of using Niv ones
    - update dependencies to Qt6 to match `pyproject.toml`
    - fix coredump on Wayland because of missing runtime dependency
    - fix build-system to use the newer `poetry-core`
    - update poetry.lock
    - make flake inputs and Niv sources poetry2nix versions match

--- flake.nix
+++ flake.nix
@@ -1,19 +1,39 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs }: (flake-utils.lib.eachDefaultSystem (
+  outputs = { self, nixpkgs, ... }@inputs: (inputs.flake-utils.lib.eachDefaultSystem (
     system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      openconnect-sso = (import ./nix { inherit pkgs; }).openconnect-sso;
+      poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+
+      openconnect-pkgs = import ./nix {
+        inherit pkgs poetry2nix;
+        sources = null; # make sure we don't mix flakes and Niv
+      };
     in
     {
-      packages = { inherit openconnect-sso; };
-      defaultPackage = openconnect-sso;
+      packages = rec {
+        inherit (openconnect-pkgs) openconnect-sso;
+
+        default = openconnect-sso;
+      };
+
+      devShells.default = openconnect-pkgs.shell;
     }
   ) // {
-      overlay = import ./overlay.nix;
+      overlays = rec {
+        default = openconnect-sso;
+
+        openconnect-sso = import ./overlay.nix;
+      };
   });
 }
