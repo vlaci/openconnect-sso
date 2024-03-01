4ab84beba3b0740a524950b3368cba00eb4d22fa
Moinak Bhattacharyya <moinakb001@gmail.com>
2024-02-29 20:21:21 -0600

    Update poetry dependency

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, flake-utils, nixpkgs }: (flake-utils.lib.eachDefaultSystem (
+  outputs = { self, flake-utils, nixpkgs, poetry2nix }: (flake-utils.lib.eachDefaultSystem (
     system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system}.extend poetry2nix.overlays.default;
       openconnect-sso = (import ./nix { inherit pkgs; }).openconnect-sso;
     in
     {
